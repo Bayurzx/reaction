1b52deef707279dd6fffbfb62aa7fd7c15c82ee2
Bayurzx <bayurzx@gmail.com>
2020-10-29 19:25:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import { Loading } from './Loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super (props);
+    //state
+    this.state = {
+      users: [],
+      loading: false
+    };
+  }
+
+  getUsers(){
+      this.setState({
+        loading: true
+      })
+
+    axios('https://api.randomuser.me/?nat=US&results=3')
+      .then(
+        response => this.setState({
+          users : response.data.results,
+          loading: false
+        })
+      );
+
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.getUsers();
+    console.log("do something");
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.loading ?
+          this.state.users.map(user  =>
+          <div>
+            <strong><p> {user.name.first} {user.name.last}</p></strong>
+            <div>{user.cell} </div>
+
+            <div>{user.dob.age} </div>
+            <div>{user.email} </div>
+            <div>{user.location.city} </div>
+            <form onSubmit={this.handleSubmit}>
+              <input type="submit" value='Load' />
+            </form>
+            <hr/>
+          </div>
+
+        ) : (
+           <Loading message = "Please wait..." />
+         )}
+       </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Loading = ({message} ) => <h1>Loading, {message} </h1>
+
+// export default Loading;
