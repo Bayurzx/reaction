1d7ac2315a1b0d33945e622ee59194c50170b7df
Bayurzx <bayurzx@gmail.com>
2020-10-30 00:41:26 +0100

    Created increment button

--- src/App.js
+++ src/App.js
@@ -1,76 +1,27 @@
 // import logo from './logo.svg';
 // import './App.css';
+// import axios from 'axios';
+// import { Loading } from './Loading';
 import React, {Component} from 'react';
-import axios from 'axios';
-import { Loading } from './Loading';
 
 class App extends Component {
-  constructor(props) {
-    super (props);
-    //state
-    this.state = {
-      users: [],
-      loading: false
-    };
-    //bind this **Note that this line was not necessary as the arrow function takes care of it**
-    //this.handleSubmit = this.handleSubmit.bind(this);
+
+  state = {
+     count : 0
   }
 
-
-  getUsers(){
-      this.setState({
-        loading: true
-      })
-
-    axios('https://api.randomuser.me/?nat=US&results=3')
-      .then(
-        response => this.setState({
-          users : [...this.state.users, ...response.data.results],
-          loading: false
-        })
-      );
-
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.getUsers();
-    console.log("do something");
-  }
-
-  componentDidMount = () => {
-    this.getUsers();
-  }
+  increment = () => {
+    this.setState({
+    count : this.state.count + 1
+  })
+}
 
   render() {
-    // Destructuring elements from this.state
-    const {loading, users} = this.state
-
     return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input type="submit" value="Load" />
-        </form>
-        <hr />
-
-        {!loading ? (
-          users.map(user  => (
-          // It seems the key property is unique and can be represented by a unique value from the data collected
-          // The key is inserted in the main div note that the main div is rewuired ti house other elements rendered
-          <div key={user.id.value}>
-            <strong><p style={{color:"#1e1e1e"}} > {user.name.first} {user.name.last}</p></strong>
-            <div>{user.cell} </div>
-
-            <div>{user.dob.age} </div>
-            <div>{user.email} </div>
-            <div>{user.location.city} </div>
-            <hr/>
-          </div>))
-
-        ) : (
-           <Loading message = "Please wait..." />
-         )}
-       </div>
+      <div>
+        <h2> Hello Counter App</h2>
+        <button onClick={this.increment}>I was clicked {this.state.count} times</button>
+      </div>
     )
   }
 }
